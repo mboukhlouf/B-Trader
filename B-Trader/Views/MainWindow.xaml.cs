a27ba3920a9a6edd0af7538bef70bb1cec5e83e7
Mohammed Boukhlouf <mohammed.boukhlouf@outlook.com>
2019-09-19 19:13:04 +0100

    Organising windows and styles.

---
+++ B-Trader/Views/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using System.Windows;
+using System.Windows.Controls;
+
+using Binance;
+using Binance.Helpers;
+namespace B_Trader
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private IEnumerable<SymbolInfo> btcPairs;
+        public BinanceClient BinanceClient { get; private set; }
+        public MainWindow()
+        {
+            InitializeComponent();
+            BinanceClient = new BinanceClient();
+        }
+
+        private async void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            ExchangeInfo exchangeInfo = await BinanceClient.GetExchangeInfoAsync();
+            btcPairs = exchangeInfo.Symbols
+                                    .Where(symbol => symbol.QuoteAsset == "BTC");
+
+            pairsListBox.ItemsSource = btcPairs;
+        }
+
+        private void SearchTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            String searchWord = ((TextBox)sender).Text;
+            pairsListBox.ItemsSource = btcPairs.Where(symbol => symbol.BaseAsset.Contains(searchWord.ToUpper()));
+        }
+    }
+}
